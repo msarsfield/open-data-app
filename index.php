efe28831aaf4c16907535b4dafd05bd67bce3aac
msarsfield <msarsfield@rogers.com>
2012-03-19 17:26:32 -0400

    updating

--- index.php
+++ index.php
@@ -17,16 +17,28 @@ $results = $db->query('
 	<script src="js/modernizr.js"></script>
 </head>
 <body>
-	
-	<ul>
-
-	<?php foreach ($results as $garden) : ?>
-		<li>
-			<a href="single.php?id=<?php echo $garden['id']; ?>"><?php echo $garden['name']; ?></a>
-		</li>
-	<?php endforeach; ?>
+      
+    <ol class="garden">
     
-	</ul>
-	
+    <?php foreach ($results as $garden) : ?>
+        <li itemscope itemtype="http://schema.org/TouristAttraction">
+            <a href="single.php?id=<?php echo $garden['id']; ?>" itemprop="name"><?php echo $garden['name']; ?></a>
+            <span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+                <meta itemprop="latitude" content="<?php echo $garden['latitude']; ?>">
+                <meta itemprop="longitude" content="<?php echo $garden['longitude']; ?>">
+            </span>
+        </li>
+    <?php endforeach; ?>
+        
+    </ol>
+      
+      <div id="map"></div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCOSF6EUJHi28FLeCSkKsQsG1gtn4vRkN4&sensor=false"></script> <!--this is Thomas', get my own-->
+	<script src="js/gardens.js"></script>
+
 </body>
 </html>
+
