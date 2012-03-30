ad092e0949ca0b9bedb714220b8feb10fec99f03
msarsfield <msarsfield@rogers.com>
2012-03-30 16:13:44 -0400

    Adding DOC BLOCK and copyright & licensing information

--- includes/users.php
+++ includes/users.php
@@ -1,7 +1,14 @@
 <?php
-
-// A self-contained file that holds all the functions we need for user authentication
-
+/**
+	*A self-contained file that holds all the functions we need for user authentication
+	*
+	*@package com.marysarsfield.open-data-app
+	*copyright 2012
+	*@author Mary Sarsfield <msarsfield@rogers.com>
+	*@link http://msarsfield.github.com/open-data-app/
+	*@license New BSD License <https://github.com/msarsfield/open-data-app/blob/master/copyright_license.txt>
+	*version 1.0.0
+*/
 
 
 function user_create ($db, $email, $password)  {
