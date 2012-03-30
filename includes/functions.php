195f682117ec22f230d23f6e3257fe1dbcb462d2
msarsfield <msarsfield@rogers.com>
2012-03-30 16:19:51 -0400

    Temporary functions file.  Will complete shortky

---
+++ includes/functions.php
@@ -0,0 +1,16 @@
+<?php  
+/**
+*NOT QUITE SURE YET... I think this is what will make the ratings work
+*
+*@package com.marysarsfield.open-data-app
+*copyright 2012
+*@author Mary Sarsfield <msarsfield@rogers.com>
+*@link http://msarsfield.github.com/open-data-app/
+*@license New BSD License <https://github.com/msarsfield/open-data-app/blob/master/copyright_license.txt>
+*version 1.0.0
+*/
+
+
+
+
+?>
