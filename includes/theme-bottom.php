b36772b0c840bf9b28b4635a299a7f407ef18b03
msarsfield <msarsfield@rogers.com>
2012-04-20 14:14:12 -0400

    foter

--- includes/theme-bottom.php
+++ includes/theme-bottom.php
@@ -1,7 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCOSF6EUJHi28FLeCSkKsQsG1gtn4vRkN4&sensor=false"></script>
-<script src="../js/gardens.js"></script>
-<script src="../js/latlng.min.js"></script>
+<script src="js/gardens.js"></script>
+<script src="js/latlng.min.js"></script>
 <footer>
 	<p>I hope you enjoy this site.  Remember to share the fruit of your labour and to support your local farmers!</p>
 </footer>
