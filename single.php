cafda62947dfaca875858198e46cc0e912d358a5
msarsfield <msarsfield@rogers.com>
2012-04-22 17:00:55 -0400

    added top and bottom

--- single.php
+++ single.php
@@ -10,7 +10,6 @@
   *version 1.0.0
   */
 
-
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 if (empty($id)) {
@@ -19,77 +18,87 @@ if (empty($id)) {
 }
 
 require_once 'includes/db.php';
-
+require_once 'includes/functions.php';
 
 $sql = $db->prepare('
-	SELECT id, name, longitude, latitude, street_address, rate_count, rate_total
-	FROM gardens
+	SELECT id, name, adr, lat, lng, rate_count, rate_total
+	FROM dinobones
 	WHERE id = :id
 ');
 
 $sql->bindValue(':id', $id, PDO::PARAM_INT);
 $sql->execute();
+$dino = $sql->fetch();
 
-$results = $sql->fetch();
-
-if (empty($results)) {
+if (empty($dino)) {
 	header('Location: index.php');
-	exit; 
+	exit;
 }
 
-$title = $garden['name'];
+$title = $dino['name'];
 
-if ($garden['rate_count'] > 0) {
-$rating = round($garden['rate_total'] / $garden['rate_count']);
+if ($dino['rate_count'] > 0) {
+	$rating = round($dino['rate_total'] / $dino['rate_count']);
 } else {
-$rating = 0;
+	$rating = 0;
 }
 
 $cookie = get_rate_cookie();
 
-require_once 'includes/theme-top.php';
+
 
 ?>
+<!DOCTYPE HTML>
+<html lang=en-ca>
+<head>
+<meta charset=utf-8>
+<title><?php if (isset($title)) { echo $title . ' · '; } ?> Ottawa Community Veggie Gardens!</title>
+<link href="css/public.css" rel="stylesheet">
+<script src="js/modernizr.dev.js"></script>
+</head>
+<body>
 
-<h1><?php echo $garden['name']; ?></h1>
+
+<h1><?php echo $dino['name']; ?></h1>
 
 <dl>
-<dt>Average Rating</dt><dd><meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter></dd>
-<dt>Address</dt><dd><?php echo $garden['street_address']; ?></dd>
-<dt>Longitude</dt><dd><?php echo $garden['longitude']; ?></dd>
-<dt>Latitude</dt><dd><?php echo $garden['latitude']; ?></dd>
+	<dt>Average Rating</dt><dd><meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter></dd>
+	<dt>Address</dt><dd><?php echo $dino['adr']; ?></dd>
+	<dt>Longitude</dt><dd><?php echo $dino['lng']; ?></dd>
+	<dt>Latitude</dt><dd><?php echo $dino['lat']; ?></dd>
 </dl>
 
 <?php if (isset($cookie[$id])) : ?>
 
 <h2>Your rating</h2>
 <ol class="rater rater-usable">
-<?php for ($i = 1; $i <= 5; $i++) : ?>
-<?php $class = ($i <= $cookie[$id]) ? 'is-rated' : ''; ?>
-<li class="rater-level <?php echo $class; ?>">★</li>
-<?php endfor; ?>
+	<?php for ($i = 1; $i <= 5; $i++) : ?>
+		<?php $class = ($i <= $cookie[$id]) ? 'is-rated' : ''; ?>
+		<li class="rater-level <?php echo $class; ?>">★</li>
+	<?php endfor; ?>
 </ol>
 
 <?php else : ?>
 
 <h2>Rate</h2>
 <ol class="rater rater-usable">
-<?php for ($i = 1; $i <= 5; $i++) : ?>
-<li class="rater-level"><a href="rate.php?id=<?php echo $garden['id']; ?>&rate=<?php echo $i; ?>">★</a></li>
-<?php endfor; ?>
+	<?php for ($i = 1; $i <= 5; $i++) : ?>
+	<li class="rater-level"><a href="rate.php?id=<?php echo $dino['id']; ?>&rate=<?php echo $i; ?>">★</a></li>
+	<?php endfor; ?>
 </ol>
 
 <?php endif; ?>
+<footer>
+	<p>I hope you that enjoy this using this application to find community growing gardens.  Please share the fruit of your labour with your friends and neighbours and remember to support your local farmers!</p>
+</footer>
 
-<h1><?php echo $results['name']; ?></h1>
-	<p>Street Address: <?php echo $results['street_address']; ?></p>
-<?php
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCOSF6EUJHi28FLeCSkKsQsG1gtn4vRkN4&sensor=false"></script>
+<script src="js/gardens.js"></script>
+<script src="js/latlng.js"></script>
 
-include 'includes/theme-bottom.php';
-
-?>
-
-
+</body>
+</html>
 
 	
 	
